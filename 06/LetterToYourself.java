fa7c9b1d5d3a9fbf5ba6b7e310dbc8e922cbab18
GLiu0925 <36462911+GLiu0925@users.noreply.github.com>
2018-02-15 12:06:48 -0500

    Add files via upload

---
+++ 06/LetterToYourself.java
@@ -0,0 +1,14 @@
+public class LetterToYourself{
+    public static void main(String[] args){
+    System.out.println("+-----------------------------------------------------------------+");
+    System.out.println("|                                                              ###|");
+    System.out.println("|                                                              ###|");
+    System.out.println("|                                                              ###|");
+    System.out.println("|                                                                 |");
+    System.out.println("|                                                                 |");
+    System.out.println("|                                             Grace Liu           |");
+    System.out.println("|                                             17 Carnation Road   |");
+    System.out.println("|                                             L3T 7V3             |"); 
+    System.out.println("+-----------------------------------------------------------------+");
+    }
+}
