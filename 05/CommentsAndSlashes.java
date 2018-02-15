9cefd35c9c77ac7855f9c92cc938a011ae594065
GLiu0925 <36462911+GLiu0925@users.noreply.github.com>
2018-02-15 12:48:05 -0500

    Add files via upload

---
+++ 05/CommentsAndSlashes.java
@@ -0,0 +1,14 @@
+//Grace Liu Feburary 15 2018
+//I was right about the two slashes.
+public class CommentsAndSlashes {
+    public static void main(String[] args) {
+    //lalalalalalalala
+    //huahahahahahaha
+
+    System.out.println("I could have code like this.");//hehe
+    //You can alsouse a comment ot "disable" or comment out a piece of code:
+    //System.out.println("this won't run.");
+    
+    System.out.println( "this will run.");
+    }
+}
