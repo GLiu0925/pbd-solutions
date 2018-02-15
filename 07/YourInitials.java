0f20e9f487fd7c023f4a7b6e459714ed4a40e0d2
GLiu0925 <36462911+GLiu0925@users.noreply.github.com>
2018-02-15 12:18:24 -0500

    Add files via upload

---
+++ 07/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials{
+    public static void main(String[] args){
+    System.out.println(" GGG   L");
+    System.out.println("G   G  L");
+    System.out.println("G      L");
+    System.out.println("GGGGG  L");
+    System.out.println("G   G  L");
+    System.out.println("G   G  L");
+    System.out.println(" GGG   LLLLL");
+    }
+}
