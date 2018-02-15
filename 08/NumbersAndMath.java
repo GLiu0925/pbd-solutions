c78ece5823bc4e00a68f5d9710e246eb08c4ce0f
GLiu0925 <36462911+GLiu0925@users.noreply.github.com>
2018-02-15 13:09:34 -0500

    Add files via upload

---
+++ 08/NumbersAndMath.java
@@ -0,0 +1,32 @@
+public class NumbersAndMath{
+    public static void main( String[] args ) {
+       // print out "I will no count my chickens.
+       System.out.println("I will now count my chickens:");
+       // 25 + 30 / 6 = 25 + 5 = 30.
+       System.out.println("Hens " + (25.00 + 30.00 / 6.00));
+       // 100 - 25 * 3 % 4 = 100 - 75 % 4 = 100 - 3 = 97.
+       System.out.println("Roosters " + (100.00 - 25.00 * 3.00 % 4.00));
+       // print out "Now I will count the eggs:.
+       System.out.println("Now I will count the eggs:");
+       //3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 = 7
+       System.out.println(3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00);
+       //3 + 2 < 5 - 7, 5 < -2, false.
+       System.out.println("Is it true that 3.00 + 2.00 < 5.00 - 7.00?");
+       
+       System.out.println(3.00 + 2.00 < 5.00 - 7.00);
+       //print....
+       System.out.println("What is 3.00 + 2.00? " + (3.00 + 2.00));
+       System.out.println("What is 5.00 - 7.00? " + (5.00 - 7.00));
+
+       System.out.println("Oh, that's why it's false.");
+
+       System.out.println("How about some more.");
+       // 5 > -2 true   
+       System.out.println("Is it greater? " + (5.00 > -2.00));
+       // 5 >= -2 true
+       System.out.println("Is it greater or equal? " + (5.00 >= -2.00));
+       // 5 <= -2 false
+       System.out.println("Is it less or equal? " + (5.00 <= -2.00));
+       }
+}
+//A floating point is a number with a decimal, it will round up to the closest whole number.
